057dde54054dd056b9da71c5242c4d3c5cdcbce5
balandzin <balandzin2602@gmail.com>
2023-12-07 20:10:22 +0300

    init commit

---
+++ HW_3/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  HW_3
+//
+//  Created by Антон Баландин on 17.11.23.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    @IBOutlet var colorfulLabel: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorfulLabel.layer.cornerRadius = 25
+        
+        changeColor()
+        
+        redValueLabel.text = string(from: redSlider)
+        greenValueLabel.text = string(from: greenSlider)
+        blueValueLabel.text = string(from: blueSlider)
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        changeColor()
+        
+        switch sender {
+        case redSlider:
+            redValueLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenValueLabel.text = string(from: greenSlider)
+        default:
+            blueValueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+    private func changeColor() {
+        colorfulLabel.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
+
